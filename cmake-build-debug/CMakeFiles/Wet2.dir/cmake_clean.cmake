40b76ddce4c4b1e837039b8bac72938f5926c112
shlomizvi09 <54794060+shlomizvi09@users.noreply.github.com>
2020-06-05 16:02:12 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Wet2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Wet2.dir/main.cpp.obj"
+  "Wet2.exe"
+  "Wet2.exe.manifest"
+  "Wet2.pdb"
+  "libWet2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Wet2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
