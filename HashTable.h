37e7385aaf25077f9b069af4fba9ba80a113ba18
shlomizvi09 <54794060+shlomizvi09@users.noreply.github.com>
2020-06-19 16:11:59 +0300

    add aux_tree to SecondTreeNodeData

--- HashTable.h
+++ HashTable.h
@@ -8,115 +8,114 @@
 #include "AVLRankTree.h"
 
 typedef enum HashTableResult {
-    HASH_SUCCESS,
-    HASH_FAILURE,
-    HASH_ALREADY_EXIST,
-    HASH_DONT_EXIST,
+  HASH_SUCCESS,
+  HASH_FAILURE,
+  HASH_ALREADY_EXIST,
+  HASH_DONT_EXIST,
 } HashTableResult;
 
 template<class Key, class Data>
 class HashTable {
-    int array_size;
-    int size;
-    AVLRankTree<Key, Data> **trees;
-public:
-    HashTable() : array_size(11), size(0) {
-        trees = new AVLRankTree<Key, Data> *[array_size];
-        for (int i = 0; i < array_size; ++i) {
-            trees[i] = new AVLRankTree<Key, Data>();
-        }
+  int array_size;
+  int size;
+  AVLRankTree<Key, Data> **trees;
+ public:
+  HashTable() : array_size(11), size(0) {
+    trees = new AVLRankTree<Key, Data> *[array_size];
+    for (int i = 0; i < array_size; ++i) {
+      trees[i] = new AVLRankTree<Key, Data>();
     }
+  }
 
-    ~HashTable() {
-        for (int i = 0; i < array_size; ++i) {
-            DeleteTreeData(trees[i]->getRoot());
-            trees[i]->cleanTree(trees[i]->getRoot());
-            delete trees[i];
-        }
-        delete [] trees;
+  ~HashTable() {
+    for (int i = 0; i < array_size; ++i) {
+      DeleteTreeData(trees[i]->getRoot());
+      trees[i]->cleanTree(trees[i]->getRoot());
+      delete trees[i];
     }
+    delete[] trees;
+  }
 
-    int HashFunc(int x, int size) {
-        return x % size;
+  int HashFunc(int x, int size) {
+    return x % size;
+  }
+
+  void DeleteTreeData(TreeNode<Key, Data> *root) {
+    if (root == nullptr)
+      return;
+    DeleteTreeData(root->getRightSon());
+    DeleteTreeData(root->getLeftSon());
+    delete root->getData();
+  }
+
+  HashTableResult search(int x, TreeNode<Key, Data> **tree_node) {
+    int tmp_cell = HashFunc(x, array_size);
+    AVLRankTreeResult tmp_result = trees[tmp_cell]->searchNode(x, tree_node);
+    if (tmp_result == AVL_SUCCESS) {
+      return HASH_ALREADY_EXIST;
     }
+    return HASH_DONT_EXIST;
+  }
 
-    void DeleteTreeData(TreeNode<Key, Data> *root) {
-        if (root == nullptr)
-            return;
-        DeleteTreeData(root->getRightSon());
-        DeleteTreeData(root->getLeftSon());
-        delete root->getData();
+  HashTableResult insert(int x, Data data) {
+    HashTableResult tmp_result = HASH_FAILURE;
+    TreeNode<Key, Data> *temp_tree_node = nullptr;
+    tmp_result = search(x, &temp_tree_node);
+    if (tmp_result == HASH_ALREADY_EXIST)
+      return HASH_ALREADY_EXIST;
+    int tmp_cell = HashFunc(x, array_size);
+    trees[tmp_cell]->add(x, data);
+    size++;
+    if (size >= array_size)
+      rehash(array_size * 2);
+    return HASH_SUCCESS;
+  }
+
+  HashTableResult remove(int x) {
+    HashTableResult tmp_result = HASH_FAILURE;
+    TreeNode<Key, Data> *temp_tree_node = nullptr;
+    tmp_result = search(x, temp_tree_node);
+    if (tmp_result == HASH_DONT_EXIST)
+      return HASH_DONT_EXIST;
+    int tmp_cell = HashFunc(x, array_size);
+    trees[tmp_cell]->remove(x);
+    size--;
+    if (size < array_size / 4 && array_size > 11)
+      rehash(array_size / 2);
+    return HASH_SUCCESS;
+  }
+
+  HashTableResult rehash(int new_size) {
+    AVLRankTree<Key, Data> **new_tree = new AVLRankTree<Key, Data> *[new_size];
+    for (int i = 0; i < new_size; ++i) {
+      new_tree[i] = new AVLRankTree<Key, Data>();
     }
-
-
-    HashTableResult search(int x) {
-        int tmp_cell = HashFunc(x, array_size);
-        AVLRankTree<Key, Data> *tmp_tree = trees[tmp_cell];
-        TreeNode<Key, Data> *tmp_node= nullptr;
-        AVLRankTreeResult tmp_result = AVL_KeyNotFound;
-        tmp_result=trees[tmp_cell]->searchNode(x,&tmp_node);
-        if(tmp_result==AVL_SUCCESS)
-            return HASH_ALREADY_EXIST;
-        return HASH_DONT_EXIST;
+    for (int i = 0; i < array_size; ++i) {
+      CopyTree(trees[i]->getRoot(), new_tree, new_size);
+      trees[i]->cleanTree(trees[i]->getRoot());
+      delete trees[i];
     }
+    delete[] this->trees;
+    this->trees = new_tree;
+    this->array_size = new_size;
+    return HASH_SUCCESS;
 
-    HashTableResult insert(int x, Data data) {
-        HashTableResult tmp_result = HASH_FAILURE;
-        tmp_result=search(x);
-        if(tmp_result==HASH_ALREADY_EXIST)
-            return HASH_ALREADY_EXIST;
-        int tmp_cell = HashFunc(x, array_size);
-        trees[tmp_cell]->add(x,data);
-        size++;
-        if(size>=array_size)
-            rehash(array_size*2);
-        return HASH_SUCCESS;
+  }
+
+  void printTable() {
+    for (int i = 0; i < array_size; ++i) {
+      trees[i]->print(In);
     }
+  }
 
-    HashTableResult remove(int x){
-        HashTableResult tmp_result = HASH_FAILURE;
-        tmp_result=search(x);
-        if(tmp_result==HASH_DONT_EXIST)
-            return HASH_DONT_EXIST;
-        int tmp_cell = HashFunc(x, array_size);
-        trees[tmp_cell]->remove(x);
-        size--;
-        if(size<array_size/4 && array_size>11)
-            rehash(array_size/2);
-        return HASH_SUCCESS;
-    }
-
-    HashTableResult rehash(int new_size){
-        AVLRankTree<Key,Data>** new_tree=new AVLRankTree<Key,Data>*[new_size];
-        for (int i = 0; i < new_size; ++i) {
-            new_tree[i] = new AVLRankTree<Key, Data>();
-        }
-        for (int i = 0; i <array_size ; ++i) {
-            CopyTree(trees[i]->getRoot(),new_tree,new_size);
-            trees[i]->cleanTree(trees[i]->getRoot());
-            delete trees[i];
-        }
-        delete [] this->trees;
-        this->trees=new_tree;
-        this->array_size=new_size;
-        return HASH_SUCCESS;
-
-    }
-
-    void printTable(){
-        for (int i = 0; i <array_size ; ++i) {
-            trees[i]->print(In);
-        }
-    }
-
-    void CopyTree(TreeNode<Key, Data> *root, AVLRankTree<Key, Data> **new_table, int new_size){
-        if(root== nullptr)
-            return;
-        CopyTree(root->getLeftSon(), new_table, new_size);
-        CopyTree(root->getRightSon(), new_table, new_size);
-        int tmp_cell = HashFunc(root->getKey(), new_size);
-        new_table[tmp_cell]->add(root->getKey(), root->getData());
-    }
+  void CopyTree(TreeNode<Key, Data> *root, AVLRankTree<Key, Data> **new_table, int new_size) {
+    if (root == nullptr)
+      return;
+    CopyTree(root->getLeftSon(), new_table, new_size);
+    CopyTree(root->getRightSon(), new_table, new_size);
+    int tmp_cell = HashFunc(root->getKey(), new_size);
+    new_table[tmp_cell]->add(root->getKey(), root->getData());
+  }
 };
 
 #endif //WET2_HASHTABLE_H
